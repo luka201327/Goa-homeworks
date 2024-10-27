12537a8a54f23f494ba37d9cd352604542172348
luka201327 <lukadateshishidze@gmail.com>
2024-10-27 18:31:52 +0400

    second commit

---
+++ level 03/classwrok/claswork.py
@@ -0,0 +1,12 @@
+name=("luka")
+surname = ("dateshize")
+age = ("11")
+animal = ("dog")
+(print) (name)
+(print) (surname)
+(print) (age)
+(print) (animal)
+
+
+
+
---
+++ level 03/homework/homework.py
@@ -0,0 +1,12 @@
+# 3) შექმენი ცვლადები,  სახელი,გვარი,ასაკი,ჰობი,ქვეყანა და  ეს ყველა ცვლადი დაპრინტეთ. 
+
+name = "luka"
+lastname = "dateshidze"
+age = 11
+hobby = "boxing"
+country = "georgia"
+
+print(name)
+print(lastname)
+print(hobby)
+print(country)
