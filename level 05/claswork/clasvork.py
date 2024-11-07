b4d485cbc93abf943238eeb24eb6c7682ee54c9a
lukadateshidze201327 <lukadateshidze2013t@gmail.com>
2024-11-07 23:10:10 +0400

    commit

---
+++ level 05/claswork/clasvork.py
@@ -0,0 +1,44 @@
+# 1
+greeting = "Hello"
+username = "student"
+age = '20'
+location = "New York"
+
+#2
+print('Welcome' + username + "!" ) 
+print(greeting + username + "!") 
+
+#3
+print("Your age is 5 " + age)  
+print("In 5 years" " you will be " + str(age) + "5" + " years old") 
+
+#4
+favoriteNumber = 7
+print("Your favorite number doubled is " + favoriteNumber * "2")  
+
+#5
+total_score = 100
+bonus_points = 10
+finalScore = "total_score" + "bonuspoint"
+print("Your final score is" + finalScore)
+
+#6
+days_in_a_week = "7"
+print("There are " + days_in_a_week * int("4") + " days in four weeks")
+
+#7
+firstName = "John"
+print("Hello" + "first + lastName" + "!")
+
+#8
+nickname = "Ace"
+print("Your nickname is: " + nickname)
+
+#10
+year = 2024
+print("Next year is " + "year" + "1")
+print("Your age next year will be" + age + "1")
+
+#11
+farewell = "Goodbye"
+print("FareWell" "username" + "! See you next time.")
---
+++ level 05/homework/homework.py
@@ -0,0 +1,14 @@
+# 1
+current_year = 2024
+print("The current year is " + "current_year")
+
+# 2
+country = "USA"
+print("You are from " + "Country")
+
+# 3
+height = 180
+print("Your height in is + height / (100) + ""meters")
+# 4
+new_name = "Alex"
+print("Hello, " + "new_name")
