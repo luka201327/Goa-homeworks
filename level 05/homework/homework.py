4e1c35d98fe9a009356d98d844ea7063f9d28c6a
lukadateshidze201327 <lukadateshidze2013t@gmail.com>
2024-11-16 20:27:10 +0400

    second commit

--- level 05/homework/homework.py
+++ level 05/homework/homework.py
@@ -12,3 +12,10 @@ print("Your height in is + height / (100) + ""meters")
 # 4
 new_name = "Alex"
 print("Hello, " + "new_name")
+
+
+
+#str()=(string -  სტრინგი)=სიტყვა,წინადადება.
+#print(gamarjoba)
+#int()=(integer - ინტეგერი)=მტელი რიცხვები - 1..2..3..4..5..6..7..8...
+#float=წილადი რიცხვები - 1,5..2,3..5,1...
