f8b56f0ec5888bb1cc1bb48c8dee41a5d8136765
luka201327 <lukadateshidze2013@gmail.com>
2024-11-22 00:23:26 +0400

    second commit

---
+++ level 07/homework/homework.py
@@ -0,0 +1,27 @@
+# შემოატანინეთ მომხარებელს სახელი გვარი და ასაკი და გამოიყენეთ ის წინადადებაში.
+name = input("Enter your name: ")
+last_name = input("Enter your last name: ")
+age = input("Enter your age: ")
+print(name + " " + last_name + " " + age)
+# მომხმარებელს შემოვატანინე სახელი, გვარი და ასაკი, სემდეგ კი გამოვიტანე ერთი წინადადებით.
+
+
+# შემოატანინეთ მომხარებელს ორი რიცხვი და მოახდინეთ მათემატიკური გამოთვლები მათზე.
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+# მომხმარებელს შემოვატანინეთ ორი რიცხვი, ამ რიცხვებზე მოვახდინე მათემატიკური ოპერაციები.
+
+
+# შემოატანინეთ მომხარებელს ემაილი და პაროლი და დაპრინტეთ ის.
+
+email = input("Enter your email: ")
+password = int(input("Enter password: "))
+
+print(email)
+print(password)
+# მომხმარებელს შემოვატანინე მისი ემაილი და შემდეგ პაროლი, საბოლოოდ კი დავპეჭდე ისინი
