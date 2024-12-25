1c1679d3917c1187ece78758d238e8db0aca8593
luka201327 <lukadateshidze2013@gmail.com>
2024-12-25 23:12:21 +0400

    commit

---
+++ level 11/claswork/cw.py
@@ -0,0 +1,60 @@
+print(10>9)
+print(12>2)
+print(13>3)
+print(14>4)
+print(15>5)
+print(16>6)
+print(17>7)
+print(18>8)
+print(19>9)
+print(100>10)
+
+
+print(1<11)
+print(2<12)
+print(3<13)
+print(4<14)
+print(5<15)
+print(6<16)
+print(7<17)
+print(8<18)
+print(9<19)
+print(10<100)
+
+
+print(11!=1)
+print(12!=2)
+print(13!=3)
+print(14!=4)
+print(15!=5)
+print(16!=6)
+print(17!=7)
+print(18!=8)
+print(19!=9)
+print(100!=10)
+ 
+
+print(11==1)
+print(12==2)
+print(13==3)
+print(14==4)
+print(15==5)
+print(16==6)
+print(17==7)
+print(18==8)
+print(19==9)
+print(100==10)
+
+
+y =("cold") 
+x = ("cold")
+
+print(y == x)
+print(y < x)
+print(y > x)
+print(y != x)
+
+
+
+
+
